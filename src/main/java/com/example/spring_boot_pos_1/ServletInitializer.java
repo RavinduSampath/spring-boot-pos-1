a1134e1479cf7c8bcef47bb7292a4a8182df9b7c
Ravindu <praneethravindu50@gmail.com>
2025-01-15 22:48:21 +0530

    project created

---
+++ src/main/java/com/example/spring_boot_pos_1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_pos_1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootPos1Application.class);
+	}
+
+}
